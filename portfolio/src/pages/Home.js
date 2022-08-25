ef426174a9d320997ecee999b58edbace85cf01b
Bre <breanna.schmidt12@gmail.com>
2022-08-25 19:44:49 -0400

    added links for icons in footer

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -40,9 +40,9 @@ function Home() {
 				<a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=new "> Feel free to contact me!</a>
    	 </p>
 		 <p>Copyright &copy; <strong>Breanna Schmidt</strong> 2022</p>
-		 <a href=""><img className="icons" src={require("../images/icons/github.png")} alt="Github logo"></img></a>
-		 <a href=""><img className="icons" src={require("../images/icons/linkedin.png")} alt="LinkedIn logo"></img></a>
-		 <a href=""><img className="icons" src={require("../images/icons/instagram.png")} alt="Instagram logo"></img></a>
+		 <a href="https://github.com/breschmidt182?tab=repositories"><img className="icons" src={require("../images/icons/github.png")} alt="Github logo"></img></a>
+		 <a href="https://www.linkedin.com/in/breanna-schmidt0711/"><img className="icons" src={require("../images/icons/linkedin.png")} alt="LinkedIn logo"></img></a>
+		 <a href="https://www.instagram.com/bre.schmidt182/"><img className="icons" src={require("../images/icons/instagram.png")} alt="Instagram logo"></img></a>
  	 </div>
 	</footer>
  </main>
