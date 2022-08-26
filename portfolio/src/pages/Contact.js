0baeefda40c8aa19169a289e8f44b3c55834bebd
Bre <breanna.schmidt12@gmail.com>
2022-08-25 21:24:15 -0400

    created most of the authentication for contact form, can't get the error to render though

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,16 +1,57 @@
+import {useState} from 'react';
+import {useForm} from '@formspree/react';
+
 function Contact() {
+	const [submit, handleSubmit] = useForm("xoqbvlgb")
+	const [formState, setFormState] = useState({name:'', email:'', message:''});
+	const {name, email, message} = formState;
+	const [error, setError] = useState('');
+
+	function validateEmail(email) {
+		var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(email).toLowerCase());
+	}
+
+	function handleChange(e) {
+		if(e.target.name === 'email') {
+			const isValid = validateEmail(e.target.value);
+			console.log(isValid);
+
+			if(!isValid) {
+				setError('Your email is invalid');
+			} else {
+				setError('')
+			}
+		} else {
+			if(!e.target.value.length) {
+				setError(`${e.target.name} is required`);
+			} else {
+				setError('');
+			}
+		}
+		setFormState({...formState, [e.target.name]: e.target.value})
+
+		if(!error) {
+			setFormState({...formState, [e.target.name]: e.target.value})
+		}
+	}
+	
+	if(submit.succeeded) {
+		return <div>Message Sent!</div>
+	}
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className='field'>
 				<label className='label'>Name</label>
 				<div className='control'>
-					<input className='input' type='text' placeholder='Enter name here'></input>
+					<input className='input' type='text' defaultValue={name} onChange={handleChange} name="name" placeholder='Enter name here'></input>
 				</div>
 			</div>
 			<div className='field'>
 				<label className='label'>Email</label>
 				<div className='control has-icons-left has-icons-right'>
-					<input className='input' type='email' placeholder='Enter email address here'></input>
+					<input className='input' type='email' defaultValue={email} onChange={handleChange} name="email" placeholder='Enter email address here'></input>
 					<span className='icon is-small is-left'>
 						<i className='fas fa-envelope'></i>
 					</span>
@@ -20,7 +61,7 @@ function Contact() {
 			<div className='field'>
 				<label className='label'>Message</label>
 				<div className='control'>
-					<textarea className='textarea' placeholder='Type message here'></textarea>
+					<textarea className='textarea' defaultValue={message} onChange={handleChange} name="message" placeholder='Type message here'></textarea>
 				</div>
 			</div>
 
