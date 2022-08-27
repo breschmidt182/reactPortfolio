adaa84f689eec7bd72c88cd5b05c9b83707f8012
Bre <breanna.schmidt12@gmail.com>
2022-08-26 22:18:16 -0400

    added pdf

--- portfolio/src/pages/Resume.js
+++ portfolio/src/pages/Resume.js
@@ -1,9 +1,20 @@
 function Resume() {
 	return (
-		<section>
-			<button></button>
-		</section>
+		<div>
+			<h1>Resume</h1>
+			<button className='pdfDownload'><a href={require("../images/webDevResume.pdf")}>Download Resume Here!</a></button>
+			<h1>Tech Stack</h1>
+			<ul>
+				<li>HTML, CSS, Javascript, jQuery</li>
+				<li>Node.js, Jest, inquirer, Express, npms</li>
+				<li>MySQL, Sequelize, Mongodb, Mongoose</li>
+				<li>Web APIs, Third-Party APIs, Server-Side APIs </li>
+			</ul>
+		</div>
+		
 	)
 }
+
+
 
 export default Resume;
