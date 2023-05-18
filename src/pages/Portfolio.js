02869cc6ed0186e11f14d0455bc73422c61d424c
Bre <breanna.schmidt12@gmail.com>
2023-05-18 15:15:34 -0400

    fixed typos

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,14 +9,14 @@ import portfolio from '../images/portfolio.png'
 function Portfolio(props) {
 	return (
 		<div className="tile is-ancestor">
-			<PortfolioCards className="tile is-parent"
+			<PortfolioCards className="tile is-parent tilt-in-top-1 "
 				title="Work Day Scheduler"
 				img= {workDayImg}
 				description="Users can keep track of things they need to do or appointments coming up using this app. This app will save any entries you make as well as change color depending on whether the event is coming up or has passed."
 				link="https://breschmidt182.github.io/work-day-scheduler/"
 				repo="https://github.com/breschmidt182/work-day-scheduler"
 			/>
-			<PortfolioCards className="tile is-parent"
+			<PortfolioCards className="tile is-parent tilt-in-top-1 "
 				title="What's For Dinner?"
 				img={whatsForDinner}
 				description="Use this app to generate new dinner recipes as well as generate a random cocktail to go with the user's meal"
@@ -24,7 +24,7 @@ function Portfolio(props) {
 				repo="https://github.com/TristinNguyen/Whats-for-Dinner"
 			/>
 
-			<PortfolioCards className="tile is-parent"
+			<PortfolioCards className="tile is-parent tilt-in-top-1 "
 				title="Event Buddy"
 				img={eventBuddy}
 				description="The idea for this app was to create something users could use to find events near them. Whether it was a concert or someone trying to find enough people to play a basketball game. Currently not fully functional due to a lack of time."
@@ -32,7 +32,7 @@ function Portfolio(props) {
 				repo="https://github.com/cmskorcz/bootcamp_project_2"
 			/>
 
-			<PortfolioCards className="tile is-parent"
+			<PortfolioCards className="tile is-parent tilt-in-top-1 "
 				title="Purrfect Pet Shop"
 				img={purrfectPetShop}
 				description="Users can use this app to shop for their cats. Instead of having to dig around for cat products on other online pet shops, there is now a shop specifically for cats"
@@ -40,7 +40,7 @@ function Portfolio(props) {
 				repo="https://github.com/cmskorcz/bootcamp_project_2"
 			/>
 
-			<PortfolioCards className="tile is-parent"
+			<PortfolioCards className="tile is-parent tilt-in-top-1 "
 				title="E-commerce Behind the Scenes "
 				img={ecommerce}
 				description="server side project for an ecommerce site"
@@ -48,7 +48,7 @@ function Portfolio(props) {
 				repo="https://github.com/breschmidt182/ecommerceBehindTheScenes"
 			/>
 
-			<PortfolioCards className="tile is-parent"
+			<PortfolioCards className="tile is-parent tilt-in-top-1 "
 				title="First Portfolio"
 				img={portfolio}
 				description="A landing page used to showcase some of the skills and projects of the Web Developer"
